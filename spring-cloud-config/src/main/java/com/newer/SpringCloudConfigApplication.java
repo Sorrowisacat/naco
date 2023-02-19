2ddc9616a1b722028efc072042d2746a435616a4
ice <11215235+hjy_2003@user.noreply.gitee.com>
2023-02-20 00:28:37 +0800

    Initial commit

--- spring-cloud-config/src/main/java/com/newer/SpringCloudConfigApplication.java
+++ spring-cloud-config/src/main/java/com/newer/SpringCloudConfigApplication.java
@@ -3,10 +3,12 @@ package com.newer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer  // 开启配置服务（提供配置服务）
 public class SpringCloudConfigApplication {
 
     public static void main(String[] args) {
